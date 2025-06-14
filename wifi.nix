3a42665f207bd419fc072d776db1c9e8507b036e
Fabian Schmitthenner <development@schmitthenner.eu>
2025-06-14 17:50:49 +0000

    add channel

--- wifi.nix
+++ wifi.nix
@@ -3,6 +3,7 @@
     enable = true;
     radios.wlp2s0.networks.wlp2s0 = {
       ssid = "My New Test Network";
+      channel = 6;
       authentication = {
         mode = "none";
 #        saePasswords = [ { passwordFile = "/run/secrets/wifi"; } ];
