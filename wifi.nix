92e2fe56c9bc8ba0801eef42c6fb16309a43470f
Fabian Schmitthenner <development@schmitthenner.eu>
2025-06-14 17:52:13 +0000

    fxup

--- wifi.nix
+++ wifi.nix
@@ -1,9 +1,9 @@
 {
   services.hostapd = {
     enable = true;
+    radios.wlp2s0.channel = 6;
     radios.wlp2s0.networks.wlp2s0 = {
       ssid = "My New Test Network";
-      channel = 6;
       authentication = {
         mode = "none";
 #        saePasswords = [ { passwordFile = "/run/secrets/wifi"; } ];
