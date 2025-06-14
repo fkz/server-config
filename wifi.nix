a1d22801508d6f41cdb67315abce2f197e4e6812
Fabian Schmitthenner <development@schmitthenner.eu>
2025-06-14 17:56:57 +0000

    use older mode

--- wifi.nix
+++ wifi.nix
@@ -5,7 +5,8 @@
     radios.wlp2s0.networks.wlp2s0 = {
       ssid = "My New Test Network";
       authentication = {
-        saePasswords = [ { passwordFile = "/run/secrets/wifi"; } ];
+        mode = "wpa2-sha256";
+        wpaPasswordFile = "/run/secrets/wifi";
         enableRecommendedPairwiseCiphers = true;
       };
     };
