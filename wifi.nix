95fcd066b55e1ef153e3f991257c02efaa34933e
Fabian Schmitthenner <development@schmitthenner.eu>
2025-06-14 18:24:29 +0000

    remove wifi as not very useful like this

--- wifi.nix
+++ wifi.nix
@@ -1,12 +1,12 @@
 {
-  services.hostapd = {
-    enable = true;
-    radios.wlp2s0.channel = 6;
-    radios.wlp2s0.networks.wlp2s0 = {
-      ssid = "My New Test Network";
-      authentication = {
-        mode = "none";
-      };
-    };
-  };
+  #services.hostapd = {
+  #  enable = true;
+  #  radios.wlp2s0.channel = 6;
+  #  radios.wlp2s0.networks.wlp2s0 = {
+  #    ssid = "My New Test Network";
+  #    authentication = {
+  #      mode = "none";
+  #    };
+  #  };
+  #};
 }
