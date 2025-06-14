f97a292e796638878b40a529d5021dc3e6b24b63
Fabian Schmitthenner <development@schmitthenner.eu>
2025-06-14 16:43:15 +0000

    Move authorized keys inline and add additional device

--- configuration.nix
+++ configuration.nix
@@ -17,7 +17,8 @@
   services.sshd.enable = true;
 
   users.users.root.openssh.authorizedKeys.keys = [
-    (builtins.readFile ./authorized)
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEfr34RLh9BEV1h8wRWykGEBIDTLrSKcJBirlupGic8zaOOQHoxKrEI1LxViFiYdWjFdAgmhA7Fjyq6q+HYKILUQqYpJ16AC2D/8Hw6juxQExVVRSjTApu1XK1MUA/vz5JKapK1v0jlC5AmHvv85REvHlxfIdnkmoWAtG4hdIxqG2Aiz9DwJN2D99ZFZr2qJqODXo2mCgraY+GZ+01rBOxvtT4f62YWxaCZx3HtiKpthU51zRduTVHIxB0qnms+HfsLANAauNhJP08vAtzdCdbquucVp1tS0pNJsENaiiNnBl/htJmQPTdoH4y9fSCjBj/vlH1ZVcmOxH2F/a7TVEV fabian@nixos"
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDIGo1MuVZL1/FEM7eM8VQoyR5JWM1QwgqZgdFPn7Hi3XF0lihkPUh1Dn7L2DdNXIfXtqM5QhqK1AOl3BCV1xxRQfrHM65EOdA0F/u++8rdz9+2Vfj00oraWMJSo3wQXwmpNW0BzduqFCzG8CQs8lN59Q7tGpYgn78LHTQRuUAet74CFYedZus4yNqgVD1rXtJfNy4uGY5Rd3cpwQ9LjQJf8NfIaaTYwgZ/GndiRei0lPF1i51L5cogsFoqQzc0BSONHMh6fepd1DrbohG558uJasK1t4S2FH8f7bkBWbz1ZkwG80kGvfUFzL4scdx3LAktn7pS1PQRFmCZf2m/NMrJ+jROmowcZRgSxru+LN2+9Shnb4xUn3CBQ5c8yJMdBsugKUxisO+KRaOxcO8R5JdcotwxSQXpOvM6ySZnxroIWk+UJj1TmprHlnGL7UulPaOAFIkv2ms7pMWxBkZnIorWj+Hy4AmN8VnVQI1ZpPnGVCuN4bL4NV5/oM+bv/S0Oyk= claptop"
   ];
 
   # networking.hostName = "nixos"; # Define your hostname.
