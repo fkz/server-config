b78a509ac4f7861e07028178aa460597fae43845
Fabian Schmitthenner <development@schmitthenner.eu>
2025-06-14 17:34:34 +0000

    fixup

--- wifi.nix
+++ wifi.nix
@@ -1,7 +1,7 @@
 {
   services.hostapd = {
     enable = true;
-    radios.wlp2s0.networks.ourwifi = {
+    radios.wlp2s0.networks.wlp2s0 = {
       ssid = "My New Test Network";
       authentication = {
         mode = "wpa3-sae";
