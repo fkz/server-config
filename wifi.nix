78493802c66718b3aea6e06535a55b4f3eddba8b
Fabian Schmitthenner <development@schmitthenner.eu>
2025-06-14 17:54:21 +0000

    use default mode

--- wifi.nix
+++ wifi.nix
@@ -5,7 +5,6 @@
     radios.wlp2s0.networks.wlp2s0 = {
       ssid = "My New Test Network";
       authentication = {
-        mode = "none";
         saePasswords = [ { passwordFile = "/run/secrets/wifi"; } ];
         enableRecommendedPairwiseCiphers = true;
       };
